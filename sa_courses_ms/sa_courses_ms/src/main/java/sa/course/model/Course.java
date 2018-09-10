39745b6173715d8dbb71412baf983bc8ef72103b
Diego Rodriguez <diefrodriguezcha@unal.edu.co>
2018-09-09 22:02:43 -0500

    added project folder

---
+++ sa_courses_ms/sa_courses_ms/src/main/java/sa/course/model/Course.java
@@ -0,0 +1,47 @@
+package sa.course.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
+@NamedQueries({@NamedQuery(name = Course.FIND_ALL, query = "SELECT u FROM Course u")})
+public class Course {
+
+    public static final String FIND_ALL = "Course.findAll";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long code;
+    private String teacher;
+    private String name;
+    private Long credits;
+
+    public Long getCode() {
+        return code;
+    }
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public String getName(){
+      return name;
+    }
+    public void setName(String name){
+      this.name = name;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getTeacher(){
+        return teacher;
+    }
+
+    public Long getCredits() {
+        return credits;
+    }
+    public void setCredits(Long credits) {
+        this.credits = credits;
+    }
+}
